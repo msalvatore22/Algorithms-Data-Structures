c73fbf0e455b7cd0a2e541a762efc3296c10f55c
Michael Salvatore <msalvatore22@gmail.com>
2021-09-28 21:34:50 -0400

    Completed simple singly linked list in python

--- Data-Structures/Linked-Lists/singly_linked_list.py
+++ Data-Structures/Linked-Lists/singly_linked_list.py
@@ -1,32 +1,91 @@
-!#/usr/bin/python3
 
 class Node:
-  def __init__(self, value=None, next):
-    self.value = value
-    self.next = None
+	def __init__(self, data=None):
+		self.data = data
+		self.next = None
 
 
 class SinglyLinkedList:
-  def __init__(self, head=None, tail=None):
-    self.head = None
-    self.tail = None
-    self.length = 0
+	def __init__(self):
+		self.head = Node()
+		self.tail = None
+		self.length = 0
 
-  
-  def push(self, value):
-    new_node = Node(value)
-    if self.head is None:
-      self.head=new_node
-      self.tail=self.head
-    
-    else:
-      self.tail.next = new_node
-      self.tail = new_node
-    
-    self.length += 1
-    return self
+	
+	def append(self, data):
+		new_node = Node(data)
+		cur = self.head
+		while cur.next != None:
+			cur = cur.next
+		cur.next = new_node
+	
+	def len(self):
+		cur = self.head
+		total = 0
+		while cur.next != None:
+			total+=1
+			cur = cur.next
+		return total
+	
+	def display(self):
+		elems =  []
+		cur_node = self.head
+		while cur_node.next!=None:
+			cur_node=cur_node.next
+			elems.append(cur_node.data)
+		
+		print(elems)
 
-  
-      
+	
+	def get(self, index):
+		if index>=self.len():
+			print("error index out of range")
+			return None
+		
+		cur_idx=0
+		cur_node=self.head
+		while True:
+			cur_node=cur_node.next
+			if cur_idx==index: return cur_node.data
+			cur_idx+=1
+	
+	def erase(self, index):
+		if index>=self.len():
+			print("error index out of range")
+			return None
+		
+		cur_idx=0
+		cur_node=self.head
+		while True:
+			last_node=cur_node
+			cur_node=cur_node.next
+			if cur_idx==index:
+				last_node.next=cur_node.next
+				return
+			cur_idx+=1
+	
+
+
+			
+
+	
+
+	
+
+my_list = SinglyLinkedList()
+my_list.display()
+my_list.append(3)
+my_list.append(6)
+my_list.append(9)
+my_list.append(2)
+my_list.display()
+print(my_list.len())
+print(my_list.get(1))
+my_list.erase(1)
+my_list.display()
+
+
+	
+			
 
 
